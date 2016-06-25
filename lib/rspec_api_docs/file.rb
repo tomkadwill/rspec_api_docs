747b2b9c065ba7510d79a5dadde9c362cbaea4f1
Tom Kadwill <tomkadwill@gmail.com>
2016-06-25 13:51:29 +0100

    Produce API Blueprint in one big file called apiary.apib. Add version to collection URL. Only runs when you run `rspec test/controllers/api

--- lib/rspec_api_docs/file.rb
+++
@@ -1,9 +0,0 @@
-class RspecApiDocs::File
-  def self.files_to_remove(files, api_docs_folder_path)
-    if files == []
-      Dir.glob(File.join(api_docs_folder_path, '*'))
-    else
-      files
-    end
-  end
-end
